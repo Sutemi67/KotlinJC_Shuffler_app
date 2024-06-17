ed7bdefc25f633e2c87e3ef7a286a71c414960bc
sutemi_pc <sutemi67@gmail.com>
2024-06-17 19:03:28 +0300

    1

--- app/src/main/java/com/example/yodatranslator/MainActivity.kt
+++ app/src/main/java/com/example/yodatranslator/MainActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.yodatranslator.ui.theme.YodaTranslatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +32,6 @@ class MainActivity : ComponentActivity() {
             YodaTranslatorTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +41,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(modifier: Modifier = Modifier) {
+    var textString by remember { mutableStateOf("Введите текст") }
+    val input by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = textString,
+            modifier = modifier
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        OutlinedTextField(
+            value = input,
+            onValueChange = { textString = textTranslator(input) },
+            singleLine = true
+        )
+    }
 }
 
-@Preview(showBackground = true)
+fun textTranslator(text: String): String {
+    val translatedText = text + "ddf"
+    return translatedText
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     YodaTranslatorTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
--- app/src/main/java/com/example/yodatranslator/Translator.kt
+++ app/src/main/java/com/example/yodatranslator/Translator.kt
@@ -4,8 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 private const val translatorUrl = "https://api.funtranslations.com"
-val retrofit =
-    Retrofit.Builder()
+val retrofit = Retrofit.Builder()
         .baseUrl(translatorUrl)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
